e8462123b8ae2576f91fa7a9e5f1416d4097a101
Garo Kechkarian <garokechkarian@Garos-iMac.local>
2022-08-16 20:08:15 -0700

    first question

---
+++ index.js
@@ -0,0 +1,13 @@
+var myQuestions = [
+    {
+        question: "Which city was the original franchise in 'The Real Housewives'?",
+        answers: {
+          a: 'RHOSLC',
+          b: 'RHOBH',
+          c: 'RHONY',
+          d: 'RHOC'
+        },
+        correctAnswer: 'd'
+      },
+  ];
+  
