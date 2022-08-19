490b8f42059b468bf8d86cbfc2d2834f32d4c9f2
Garo Kechkarian <garokechkarian@Garos-iMac.local>
2022-08-18 21:30:21 -0700

    almost done with skeleton

--- game.js
+++ game.js
@@ -58,7 +58,7 @@ getNewQuestion = () => {
     if(availableQuestion.length === 0|| questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign("/end,html")
+        return window.location.assign("/end.html")
     }
 
     questionCounter++
