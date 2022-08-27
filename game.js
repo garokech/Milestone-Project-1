125526d7b02d0139d456be90126ba45a6b3bfe88
Garo Kechkarian <garokechkarian@Garos-iMac.local>
2022-08-27 13:59:10 -0700

    take3

--- game.js
+++ game.js
@@ -116,7 +116,7 @@ getNewQuestion = () => {
     if(availableQuestion.length === 0|| questionCounter > maxQuestions){
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign("/end.html")
+        return window.location.assign("./end.html")
     }
 
     questionCounter++
